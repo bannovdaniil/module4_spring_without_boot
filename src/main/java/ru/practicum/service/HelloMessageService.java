1f9ff71ded8af7ffb280cbc129d11303a53213a0
bannovdaniil <baddan@mail.ru>
2022-08-11 22:35:08 +0300

    Add: логирование

--- src/main/java/ru/practicum/service/HelloMessageService.java
+++ src/main/java/ru/practicum/service/HelloMessageService.java
@@ -1,10 +1,12 @@
 package ru.practicum.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class HelloMessageService {
     private final String message;
 
@@ -14,6 +16,7 @@ public class HelloMessageService {
     }
 
     public String getHelloMessage() {
+        log.info("getHelloMessage: {}", message);
         return message;
     }
 }
