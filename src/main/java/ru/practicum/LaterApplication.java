cc4319e0eecf4294df296bd755a5fe321dd0faaa
bannovdaniil <baddan@mail.ru>
2022-08-11 16:37:03 +0300

    refactoring: to DispatcherServlet

--- src/main/java/ru/practicum/LaterApplication.java
+++ src/main/java/ru/practicum/LaterApplication.java
@@ -4,6 +4,8 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class LaterApplication {
     private static final int PORT = 8080;
@@ -14,8 +16,15 @@ public class LaterApplication {
 
         Context tomcatContext = tomcat.addContext("", null);
 
-        Wrapper testServletWrapper = Tomcat.addServlet(tomcatContext, "testServlet", new TestServlet());
-        testServletWrapper.addMapping("/test");
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.setServletContext(tomcatContext.getServletContext());
+        applicationContext.scan("ru.practicum");
+        applicationContext.refresh();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(applicationContext);
+        Wrapper dispatcherWrapper = Tomcat.addServlet(tomcatContext, "dispatcher", dispatcherServlet);
+        dispatcherWrapper.addMapping("/");
+        dispatcherWrapper.setLoadOnStartup(1);
 
         tomcat.start();
     }
